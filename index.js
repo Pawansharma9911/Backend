e5ead15fd90a8bb8426d0d5ac3ed83e727b1d58d
Pawansharma9911 <124700568+Pawansharma9911@users.noreply.github.com>
2025-12-16 15:11:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const url = process.env.MONGO_URL;
 
 const app = express();
 app.use(cors({
-   origin: ["https://dashboard-swart-alpha-72.vercel.app", "http://localhost:3000"],
+   origin: ["https://dashboard-swart-alpha-72.vercel.app", "https://frontend-omega-lovat-96.vercel.app"],
    credentials: true,
  }));
 app.use(bodyParser.json());
